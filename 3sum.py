f6ccdc120a110ac3cfd8b9ab560d22708256d794
Raj Mehta <raj.mehtajj@gmail.com>
2023-07-16 01:59:19 -0600

    Initial commit

---
+++ 3sum.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        return [[3,4], [44,55]]
+
+
+s = Solution()
+nums = [-1,0,1,2,-1,-4]
+print(s.threeSum(nums))
